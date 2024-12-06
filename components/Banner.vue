c1a64530b55d2207ff26bc854419a35278893d8d
noor3030 <nnorkrka@gmail.com>
2024-12-06 14:00:13 +0300

    Refactor responsive design in Banner and Navbar components

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,22 +1,61 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col space-y-4 text-center">
-    <h1 class="text-2xl font-bold text-white">
-      Transform your business with our
-    </h1>
-    <h1
-      class="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-custom-pink to-custom-purple"
+  <div
+    class="flex lg:justify-between lg:content-between lg:items-center lg:px-20"
+  >
+    <div
+      class="flex flex-col space-y-4 text-center lg:text-left lg:w-1/2 lg:space-y-6"
     >
-      creative marketing solutions!
-    </h1>
-    <p class="text-white font-extralight text-sm">
-      Welcome to Focus Marketing Solutions! We're a creative team dedicated to
-      driving your business forward with innovative marketing strategies. From
-      digital marketing to social media management, we're here to help you
-      succeed. Let's build something extraordinary together
-    </p>
-    <button class="text-onPrimary bg-primary rounded-full py-4 w-full" >Join us</button>
-    <button class="border border-white rounded-full w-full py-4 text-white">Contact us</button>
+      <h1 class="text-2xl font-bold text-white lg:hidden">
+        Transform your business with our
+      </h1>
+      <h1
+        class="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-custom-pink to-custom-purple lg:hidden"
+      >
+        creative marketing solutions!
+      </h1>
+
+      <h1 class="font-bold text-white text-[45px] hidden lg:block">
+        Transform your business with our
+        <span
+          class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-custom-pink to-custom-purple"
+        >
+          creative marketing solutions!
+        </span>
+      </h1>
+
+      <p class="text-white font-extralight text-sm lg:text-base">
+        Welcome to Focus Marketing Solutions! We're a creative team dedicated to
+        driving your business forward with innovative marketing strategies. From
+        digital marketing to social media management, we're here to help you
+        succeed. Let's build something extraordinary together
+      </p>
+      <div class="flex flex-col space-y-4 lg:space-y-0 lg:flex lg:flex-row lg:space-x-4">
+        <button
+          class="border text-white border-white text-[14px] p-3 px-6 rounded-3xl"
+        >
+          Contact us
+        </button>
+
+        <button class="bg-primary text-white text-[14px] p-3 px-6 rounded-3xl">
+          Join us
+        </button>
+      </div>
+
+      <img
+        src="../public/banner/banner3.png"
+        alt=""
+        class="w-80 brightness-50 hidden lg:block"
+      />
+    </div>
+    <div class="lg:flex flex-col hidden space-y-6">
+      <img src="../public/banner/banner1.png" alt="" class="w-[400px]" />
+      <img
+        src="../public/banner/banner2.png"
+        alt=""
+        class="w-[200px] brightness-50 rounded-lg"
+      />
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col space-y-16">
-    <div class="px-4 flex flex-col space-y-16">
+  <div class="flex flex-col space-y-16 lg:space-y-24">
+    <div class="px-4 flex flex-col space-y-16 lg:space-y-24">
       <Banner />
       <Partners />
     </div>
     <div class="relative">
       <img src="/Lines.svg" alt="" class="w-full h-auto object-cover" />
-      <div class="absolute top-1 left-0 right-0 flex flex-col space-y-16">
+      <div class="absolute top-1 left-0 right-0 flex flex-col space-y-16 lg:space-y-24">
         <AboutUs />
         <Services />
         <Philosophy />
