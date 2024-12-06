8a80f55f5e8d3d0642e5924a95bf2325b01a9feb
noor3030 <nnorkrka@gmail.com>
2024-12-06 21:54:31 +0300

    Refactor Philosophy component for improved responsiveness and layout

--- components/Philosophy.vue
+++ components/Philosophy.vue
@@ -50,7 +50,7 @@ function handleIntersection(entries: IntersectionObserverEntry[]) {
       animateValue(obj2, 0, counters[1].number, 5000);
       animateValue(obj3, 0, counters[2].number, 5000);
       animateValue(obj4, 0, counters[3].number, 5000);
-      observer?.disconnect(); 
+      observer?.disconnect();
     }
   });
 }
@@ -75,29 +75,39 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="flex flex-col space-y-4 px-4 md:px-8 text-left">
-    <ul class="list-disc px-4  border-b border-b-white pb-2">
-      <li class="text-primary text-left">The company's philosophy</li>
+  <div class="flex flex-col space-y-4 lg:space-y-10 px-4 md:px-8 text-left lg:px-20 ">
+    <ul class="list-disc px-4 border-b border-b-white pb-2">
+      <li class="text-primary text-left ">The company's philosophy</li>
     </ul>
-    <h1 class="text-3xl text-primary">
-      We bear the responsibility of developing the sector.
-    </h1>
-    <p class="text-base text-description font-extralight">
-      Elevate your brand with our comprehensive marketing solutions, including
-      Digital Marketing, Content Creation, and Social Media Strategy. Enhance
-      visibility through expert SEO and PPC management
-    </p>
-    <div class="grid grid-cols-2 gap-5 counter-section">
-      <div v-for="(counter, index) in counters" :key="index">
-        <h1 class="text-[27px] text-white font-extralight">
-          {{
-            index === 0 ? obj1 : index === 1 ? obj2 : index === 2 ? obj3 : obj4
-          }}
-          +
+    <div class="flex flex-col space-y-4 lg:space-y-0   text-left lg:flex-row lg:justify-between">
+      <div class="lg:w-1/3 lg:flex lg:flex-col lg:space-y-10 	">
+        <h1 class="text-3xl text-primary lg:text-[62px] lg:leading-none">
+          We bear the responsibility of developing the sector.
         </h1>
-        <h1 class="mt-4 text-[18px] text-description">
-          {{ counter.title }}
-        </h1>
+        <p class="text-base text-description font-extralight lg:text-[24px] lg:leading-8">
+          Elevate your brand with our comprehensive marketing solutions,
+          including Digital Marketing, Content Creation, and Social Media
+          Strategy. Enhance visibility through expert SEO and PPC management
+        </p>
+      </div>
+      <div class="grid grid-cols-2 lg:grid-cols-1 gap-5 counter-section">
+        <div v-for="(counter, index) in counters" :key="index">
+          <h1 class="text-[27px] text-white font-extralight lg:text-[48px] lg:font-light">
+            {{
+              index === 0
+                ? obj1
+                : index === 1
+                ? obj2
+                : index === 2
+                ? obj3
+                : obj4
+            }}
+            +
+          </h1>
+          <h1 class="mt-4 lg:mt-1 text-[18px] text-description">
+            {{ counter.title }}
+          </h1>
+        </div>
       </div>
     </div>
   </div>
