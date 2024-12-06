1a14507ebef2a4a994361374eed4f4536c22782b
noor3030 <nnorkrka@gmail.com>
2024-12-06 21:42:39 +0300

    Refactor responsive design in Services component

--- components/Services.vue
+++ components/Services.vue
@@ -33,18 +33,18 @@ const services = [
 </script>
 
 <template>
-  <div class="px-4 space-y-4 text-center flex flex-col">
-    <h1 class="text-2xl text-white font-extralight">Our services</h1>
-    <p class="text-description font-extralight text-base">
+  <div class="px-4 space-y-4 lg:space-y-10 text-center flex flex-col">
+    <h1 class="text-2xl text-white font-extralight lg:font-light lg:text-[48px]">Our services</h1>
+    <p class="text-description font-extralight text-base lg:text-[18px]">
       ransforming your online presence with innovative digital strategies
     </p>
 
-    <div class="grid grid-cols-2 gap-4">
+    <div class="grid grid-cols-2 gap-4 lg:grid-cols-3">
       <div
         v-for="service in services"
-        class="flex flex-col justify-center items-center text-center space-y-4"
+        class="flex flex-col justify-center items-center text-center space-y-4 lg:space-y-3"
       >
-        <div class="bg-white rounded-2xl p-1 w-full md:w-1/2 flex items-center justify-center content-center">
+        <div class="bg-white rounded-2xl p-1 w-full md:w-1/2 flex items-center justify-center content-center lg:w-fit">
           <div
             class="bg-primary rounded-full flex justify-center items-center w-32 h-32"
           >
