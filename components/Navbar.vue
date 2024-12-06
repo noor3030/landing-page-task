6ebbbe3c5cd6f31fc937659f23ec90619a191606
noor3030 <nnorkrka@gmail.com>
2024-12-06 13:22:29 +0300

    Refactor Navbar component to improve responsiveness and add mobile drawer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,8 +16,38 @@ const pages = [
 </script>
 
 <template>
-  <nav class="px-4 py-10">
-    <div class="flex justify-between items-center">
+  <nav class="px-4 py-10 lg:px-20">
+    <!-- desktop nav -->
+    <div class="lg:flex justify-between items-center hidden">
+      <img src="../public/logo/logo.png" alt="logo" class="w-24" />
+      <ul class="flex space-x-6 items-center font-light text-[14px]">
+        <li v-for="page in pages" :key="page.name" class="text-white">
+          <nuxt-link :to="page.link" class="flex items-center"
+            >{{ page.name }}
+            <p
+              class="bg-primary px-2.5 text-sm rounded-xl ml-2"
+              v-if="page.numbers"
+            >
+              {{ page.numbers }}
+            </p></nuxt-link
+          >
+        </li>
+      </ul>
+      <div class="flex space-x-4">
+        <button
+          class="border text-white border-white text-[14px] p-3 px-6 rounded-3xl"
+        >
+          Contact us
+        </button>
+
+        <button class="bg-primary text-white text-[14px] p-3 px-6 rounded-3xl">
+          Join us
+        </button>
+      </div>
+    </div>
+
+    <!-- mobile drawer -->
+    <div class="flex justify-between items-center lg:hidden">
       <span
         class="material-icons text-white cursor-pointer"
         @click="toggleDrawer"
@@ -27,7 +57,7 @@ const pages = [
     </div>
     <div
       v-if="isDrawerOpen"
-      class="fixed inset-0 bg-gray-800 bg-opacity-50 z-50"
+      class="fixed inset-0 bg-gray-800 bg-opacity-50 z-50 lg:hidden"
     >
       <div
         class="fixed left-0 py-10 top-0 w-64 h-full bg-background shadow-lg z-50"
